5f9e64589cbb57cecc34a1b41b5b78bdcf12b588
nordraw <nikita.tretyak093@gmail.com>
2022-02-03 18:03:24 +0200

    Adding Is Module Enabled Option

--- Controller/Index/Listing.php
+++ Controller/Index/Listing.php
@@ -4,20 +4,33 @@ namespace Encomage\Books\Controller\Index;
 
 use \Magento\Framework\App\Action\HttpGetActionInterface;
 use \Magento\Framework\View\Result\PageFactory;
+use \Encomage\Books\Helper\Data as HelperData;
+use \Magento\Backend\Model\View\Result\RedirectFactory;
 
 class Listing implements HttpGetActionInterface
 {
     protected $pageFactory;
+    protected $helperData;
+    protected $redirectFactory;
 
     public function __construct(
-        PageFactory $pageFactory
+        PageFactory $pageFactory,
+        HelperData  $helperData,
+        RedirectFactory $redirectFactory
     )
     {
         $this->pageFactory = $pageFactory;
+        $this->helperData = $helperData;
+        $this->redirectFactory = $redirectFactory;
     }
 
     public function execute()
     {
-        return $this->pageFactory->create();
+        if ($this->helperData->getGeneralConfig('enable')) {
+            return $this->pageFactory->create();
+        }
+        $redirect = $this->redirectFactory->create();
+        $redirect->setUrl('/');
+        return $redirect;
     }
 }
